e8b863e2896f604b9aa5d92fdb24f9a078567b2d
risonakamo <hmykanae@gmail.com>
2025-06-14 12:46:18 -0400

    grouping time entries

--- lib/ttt/day_container.go
+++ lib/ttt/day_container.go
@@ -2,21 +2,29 @@
 
 package ttt
 
-import "time"
+import (
+	"maps"
+	"slices"
+	"sort"
+	"time"
+)
+
+// day containers keyed by date key
+type DayContainerDict map[string]*DayContainer
 
 // container of time entries
 type DayContainer struct {
-    Id string
-
-    // this day as a string
+    // this day as a string. this should be unique
     // 2025/01/02
     DateKey string
 
     Entries []*TimeEntry
+
+    // seconds
     TotalDuration int64
 }
 
-// compute the day string of a date. before hour marks the next day. if task occurs before
+// compute the day string of a date. before hour marks the next day. if date occurs before
 // this hour, it counts as the previous day (if past midnight)
 func computeDate(unixTime int64,beforeHour int) string {
     var entryDate time.Time=time.Unix(unixTime,0)
@@ -26,4 +34,58 @@ func computeDate(unixTime int64,beforeHour int) string {
     }
 
     return entryDate.Format("2006/01/02")
+}
+
+// group time entries into corresponding day containers
+func groupTimeEntries(entries []*TimeEntry,beforeHour int) []*DayContainer {
+    var dayContainers DayContainerDict=DayContainerDict{}
+
+    var entry *TimeEntry
+    for _,entry = range entries {
+        var entryDate string=computeDate(entry.TimeStart,beforeHour)
+
+        var in bool
+        _,in=dayContainers[entryDate]
+
+        if !in {
+            dayContainers[entryDate]=&DayContainer{
+                DateKey: entryDate,
+                Entries: []*TimeEntry{},
+                TotalDuration: 0,
+            }
+        }
+
+        dayContainers[entryDate].Entries=append(
+            dayContainers[entryDate].Entries,
+            entry,
+        )
+
+        if entry.Duration>0 {
+            dayContainers[entryDate].TotalDuration+=entry.Duration
+        }
+    }
+
+    return slices.Collect(maps.Values(dayContainers))
+}
+
+// sort day containers in place based on date key
+func sortDayContainers(dayContainers []*DayContainer) {
+    sort.Slice(dayContainers, func(i int, j int) bool {
+        // Parse the date keys
+        var di time.Time
+        var dj time.Time
+        var e1 error
+        var e2 error
+
+        di, e1 = time.Parse("2006/01/02", dayContainers[i].DateKey)
+        dj, e2 = time.Parse("2006/01/02", dayContainers[j].DateKey)
+
+        if e1 != nil || e2 != nil {
+            // If parsing fails, fallback to string compare descending
+            return dayContainers[i].DateKey > dayContainers[j].DateKey
+        }
+
+        // Return true if i is after j (want descending order)
+        return di.After(dj)
+    })
 }
--- lib/ttt/day_container_test.go
+++ lib/ttt/day_container_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"testing"
 	"time"
+
+	"github.com/k0kubun/pp/v3"
 )
 
 func Test_timeEntryDate(t *testing.T) {
@@ -29,4 +31,51 @@ func Test_timeEntryDate(t *testing.T) {
             computeDate(date.Unix(),beforeHour),
         )
     }
+}
+
+func Test_groupTimeEntries(t *testing.T) {
+    var entries []*TimeEntry = []*TimeEntry{
+        {
+            Id:        "1",
+            Title:     "Morning Task",
+            TimeStart: time.Date(2025, 6, 10, 7, 30, 0, 0, time.Local).Unix(), // Before 8 AM
+            TimeEnd:   time.Date(2025, 6, 10, 8, 30, 0, 0, time.Local).Unix(),
+            Duration:  3600,
+        },
+        {
+            Id:        "2",
+            Title:     "Afternoon Task",
+            TimeStart: time.Date(2025, 6, 10, 14, 0, 0, 0, time.Local).Unix(),
+            TimeEnd:   time.Date(2025, 6, 10, 16, 0, 0, 0, time.Local).Unix(),
+            Duration:  7200,
+        },
+        {
+            Id:        "3",
+            Title:     "Overnight Task",
+            TimeStart: time.Date(2025, 6, 9, 23, 0, 0, 0, time.Local).Unix(),
+            TimeEnd:   time.Date(2025, 6, 10, 1, 0, 0, 0, time.Local).Unix(),
+            Duration:  7200,
+        },
+        {
+            Id:        "4",
+            Title:     "Unfinished Task",
+            TimeStart: time.Date(2025, 6, 11, 10, 0, 0, 0, time.Local).Unix(),
+            TimeEnd:   -1,
+            Duration:  -1,
+        },
+        {
+            Id:        "5",
+            Title:     "Another Morning Task",
+            TimeStart: time.Date(2025, 6, 12, 6, 45, 0, 0, time.Local).Unix(),
+            TimeEnd:   time.Date(2025, 6, 12, 7, 15, 0, 0, time.Local).Unix(),
+            Duration:  1800,
+        },
+    }
+
+    var beforeHour int=8
+
+    result:=groupTimeEntries(entries,beforeHour)
+    sortDayContainers(result)
+
+    pp.Println(result)
 }
